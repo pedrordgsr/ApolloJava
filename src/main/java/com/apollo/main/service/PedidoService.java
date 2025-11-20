1c2417669db7025d9fd68c03e794e22bbdbf55f2
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2025-11-20 17:03:04 -0300

    fix: ordenar pedidos por data de emissão na listagem

--- src/main/java/com/apollo/main/service/PedidoService.java
+++ src/main/java/com/apollo/main/service/PedidoService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -227,7 +228,7 @@ public class PedidoService {
     }
 
     public Page<PedidoResponseDTO> getAllPedidos(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "dataEmissao"));
         Page<Pedido> pedidoPage = pedidoRepository.findAll(pageable);
         return pedidoPage.map(PedidoResponseDTO::new);
     }
